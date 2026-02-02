1d09d231855515aeaf23369cb927fe5cd347984a
Md. Jubayer Hossain <mjh84349@gmail.com>
2026-02-02 10:42:05 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        <button className="btn btn-primary">Click</button>
+      </div>
+    </div>
+  );
+}
